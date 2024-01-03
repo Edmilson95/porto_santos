57a09d4df6a9a1c7df7914737b7798abf72cdd24
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2024-01-03 17:45:18 -0300

    criando filtro para as requisicoes

--- src/main/java/com/santos/porto/infra/security/TokenService.java
+++ src/main/java/com/santos/porto/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.santos.porto.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.santos.porto.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,21 @@ public class TokenService {
         } catch (JWTCreationException exception){
             // Invalid Signing configuration / Couldn't convert Claims.
             throw new RuntimeException("Erro ao gerar token jwt.", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    // specify an specific claim validations
+                    .withIssuer("API - Porto Santos")
+                    // reusable verifier instance
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("TokenJWT inválido ou expirado");
         }
     }
 
