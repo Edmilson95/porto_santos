aabf37bc041c004783933a6d641151d59e4da301
Edmilson95 <edmilson.svic@gmail.com>
2024-01-10 18:11:03 -0300

    testando o controle de acesso

--- src/main/java/com/santos/porto/TesteApplication.java
+++ src/main/java/com/santos/porto/TesteApplication.java
@@ -9,5 +9,5 @@ public class TesteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TesteApplication.class, args);
 	}
-
+ 
 }
--- src/main/java/com/santos/porto/infra/security/SecurityFilter.java
+++ src/main/java/com/santos/porto/infra/security/SecurityFilter.java
@@ -24,6 +24,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("Chamando filtro");
         var tokenJWT = recuperarToken(request);
 
         if (tokenJWT != null) {
@@ -32,6 +33,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
             var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
+            System.out.println("LOGADO NA REQUISICAO");
         }
 
         filterChain.doFilter(request, response);
