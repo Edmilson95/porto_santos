37961d868ed3d203554d39dc9a5498e6cafa434e
Edmilson95 <edmilson.svic@gmail.com>
2024-03-04 17:58:32 -0300

    adicionando teste automatizado do VisitaController

---
+++ src/test/java/com/santos/porto/controller/VisitaControllerTest.java
@@ -0,0 +1,22 @@
+package com.santos.porto.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@
+class VisitaControllerTest {
+
+    @Test
+    @DisplayName("Deveria devolver codigo http 400 quando informacoes estao invalidas")
+    void agendar_cenario1() {
+
+    }
+
+    @Test
+    void cancelar() {
+    }
+}
