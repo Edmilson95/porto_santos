0253a38c9123b3d2d47d29792c64afbc9eef774d
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2024-03-21 16:05:37 -0300

    teste classe visita

--- src/test/java/com/santos/porto/controller/VisitaControllerTest.java
+++ src/test/java/com/santos/porto/controller/VisitaControllerTest.java
@@ -2,21 +2,36 @@ package com.santos.porto.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@
+@AutoConfigureMockMvc
 class VisitaControllerTest {
 
-    @Test
-    @DisplayName("Deveria devolver codigo http 400 quando informacoes estao invalidas")
-    void agendar_cenario1() {
-
-    }
+    @Autowired
+    private MockMvc mvc;
 
     @Test
-    void cancelar() {
+    @DisplayName("Deveria devolver codigo http 500 quando informacoes estao invalidas")
+    @WithMockUser
+    void agendar_cenario1() throws Exception {
+        var jsonInvalido = "{ \"campoInvalido\": \"valorInvalido\" }";
+
+        var response = mvc.perform(post("/visitas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonInvalido))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
